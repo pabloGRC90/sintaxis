b9b0b20cc0f32984fb7e33b74ae2f7c3952d0312
usuario <usuario@MacBook-Pro-de-usuario.local>
2019-10-14 17:22:44 +0200

    primera subida

---
+++ js/alert.js
@@ -0,0 +1,6 @@
+//alert("cual es tu nombre?");
+//prompt("cual es tu nombre?");
+//confirm("cual es tu nombre?");
+//document.write("escribiendo en la página directamente");
+console.log("mensaje de consola mío");
+console.error("mensaje de error mío");
